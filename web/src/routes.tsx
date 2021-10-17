807b6442bab7b22b6f3b1b025f6357012d2717d7
nathan nascimento <nathasilvanascimento@gmail.com>
2021-10-16 23:22:19 -0300

    subindo a parte web

---
+++ web/src/routes.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { Landing } from './pages/Landing'
+import { OrphanagesMap } from './pages/OrphanagesMap';
+
+export function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Landing}  />
+        <Route  path="/app" component={OrphanagesMap}  />
+      </Switch>
+    </BrowserRouter>
+  );
+}
